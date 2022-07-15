f34ccf3b67dd0781502463dc5807c6ca0d30e747
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-15 23:39:48 +0530

    START: Welcome to the BST Program

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java_Day-15_BinarySearchTree_BST
---
+++ src/Com/BridgeLabz/BinarySearchTree/MainBst.java
@@ -0,0 +1,7 @@
+package Com.BridgeLabz.BinarySearchTree;
+
+public class MainBst {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the binary search tree program");
+    }
+}
