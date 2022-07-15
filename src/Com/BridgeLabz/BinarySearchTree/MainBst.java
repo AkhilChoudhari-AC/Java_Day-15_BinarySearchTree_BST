d7ba82c8f72c3dbf61e06377fa4c45c10780b846
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-15 23:46:38 +0530

    ADDED: Ability to create BST by adding nodes

--- src/Com/BridgeLabz/BinarySearchTree/MainBst.java
+++ src/Com/BridgeLabz/BinarySearchTree/MainBst.java
@@ -2,6 +2,25 @@ package Com.BridgeLabz.BinarySearchTree;
 
 public class MainBst {
     public static void main(String[] args) {
-        System.out.println("Welcome to the binary search tree program");
+        BinaryTree bst = new BinaryTree();
+
+        /*
+         * initial node root will pointing to null here root is empty
+         */
+        Node root = null;
+
+        /*
+         * 2.inserting the values in the root by calling the insert method
+         */
+        root = bst.insert(root, 56);
+
+        root = bst.insert(root, 30);
+
+        root = bst.insert(root, 70);
+
+        /*
+         * 3.here printing the root values
+         */
+        bst.print(root);
     }
 }
